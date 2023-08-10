d107f632a6660ed0881c9dab6e7230cbc530b7cf
MrWolf0 <nassermessi33@gmail.com>
2023-08-10 13:55:00 +0300

    c programming start

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<unistd.h>
+#include <string.h>
+
+/**
+ * main - start point of the program
+ *
+ * Return: always must be 0 in case you are using int if you using void
+ *
+ *  no need to return keyword
+ *
+* here just using strlen from string.h to calculate buffer size
+*
+*then write the string with the same size that assign to the buffer
+*
+*and using 1 as  file descriptor of  standard output
+ */
+int main(void)
+{
+char str_test[] = "and that piece of art is useful\" -"
+" Dora Korpar, 2015-10-19";
+int buffer =  strlen(str_test);
+
+write(1, str_test, buffer);
+
+return (1);
+}
+
