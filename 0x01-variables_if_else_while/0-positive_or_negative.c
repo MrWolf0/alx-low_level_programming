9e51d6feb0d3335a1699131a4438d8684a23be62
MrWolf0 <nassermessi33@gmail.com>
2023-08-11 12:34:44 +0300

    c programming if else condition

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -21,16 +21,16 @@ n = rand() - RAND_MAX / 2;
 /* if condition check n value positive , negative or zero */
 if (n > 0)
 {
-printf("%d number is psitive ", n);
+printf("%d number is psitive\n ", n);
 }
 else if (n == 0)
 {
-printf("%d is zero ", n);
+printf("%d is zero\n ", n);
 
 }
 else
 {
-printf("%d is negative ", n);
+printf("%d is negative\n ", n);
 }
 
 return (0);
