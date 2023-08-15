85518c9e8c864544c67830375fa44b3876ad95d1
MrWolf0 <nassermessi33@gmail.com>
2023-08-15 18:28:32 +0300

    functions and nested loop

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,34 +1,38 @@
 #include "main.h"
 /**
- *  jack_bauer - print time of day in formate h:min
- * Description: prints time in formate h:min
+ *  times_table - print row*column for 9 times for 9 column
+ * Description: print row*column for 9 times for 9 column
  * Return: Always 0.
  */
 void times_table(void)
 {
-int final_res=0;
-int hours , minutes;
-for (hours = 0; hours <= 9 ; hours++)
+int final_res = 0;
+int row, column;
+for (row = 0; row <= 9 ; row++)
 {
-for (minutes = 0; minutes <= 9; minutes++)
+for (column = 0; column <= 9; column++)
 {
-final_res = hours*minutes;
-if (final_res > 9){
-if(minutes <= 9){
+final_res = row * column;
+if (final_res > 9)
+{
+if (column <= 9)
+{
 _putchar(',');
 _putchar(' ');
 }
-_putchar(final_res/10+'0');
-_putchar(final_res%10+'0');
+_putchar(final_res / 10 + '0');
+_putchar(final_res % 10 + '0');
 
 }
-else{
-if(minutes <= 9){
+else
+{
+if (column <= 9 && column != 0)
+{
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
 }
-_putchar(final_res+'0');
+_putchar(final_res + '0');
 
 }
 }
