da02b0adead82b8341d0907ee281c4b8bb1ec204
MrWolf0 <nassermessi33@gmail.com>
2023-08-16 00:16:53 +0300

    functions and nested loop

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,21 +1,21 @@
 #include "main.h"
 /**
- *  print_times_table - print row*column for n times for n column
- * Description: print row*column for 9 times for 9 column
- * Return: Always 0.
+ *print_times_table - print row*column for n times for n column
+  *@number: test
+  * Return: Always 0.
  */
 void print_times_table(int number)
 {
-if (number <= 15)
-{
 int final_res = 0;
 int row, column;
+if (number <= 15)
+{
 for (row = 0; row <= number; row++)
 {
 for (column = 0; column <= number; column++)
 {
 final_res = row * column;
-if (final_res > 9 && final_res < 100)
+if (final_res > 10 && final_res < 100)
 {
 if (column <= number)
 {
@@ -47,9 +47,7 @@ _putchar(' ');
 _putchar(' ');
 }
 _putchar(final_res + '0');
-}
-}
+}}
 _putchar('\n');
-}
-}
-}
+}}}
+
