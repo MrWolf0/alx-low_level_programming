2a4ee6e84c02c19add570684e0a0f8c406bf34b2
MrWolf0 <nassermessi33@gmail.com>
2023-08-15 18:37:28 +0300

    functions and nested loop

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *  add - sum two integer values
+* @num1:  is first integer
+* @num2:  is second integer
+ * Return: Always 0.
+ */
+int add(int num1, int num2)
+{
+int sum;
+sum = num1 + num2;
+return (sum);
+}
+
---
+++ 0x02-functions_nested_loops/add-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - start point of the program
+ *
+ * Return: always must be 0 in case you are using int if you using void
+ *
+ *  no need to return keyword
+ *
+ * calling main.h and compile 2 files together with gcc
+*the main and the file contain the declaration of putchar function
+*/
+int main()
+{
+printf("%d",add(89,9));
+    return (0);
+}
+
