489a5c85e537ebd3b776c45686d8c75518af1a40
MrWolf0 <nassermessi33@gmail.com>
2023-08-16 23:38:59 +0300

    functions and nested loop

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,26 +6,32 @@
 
 /**
  *main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
- *       separated by a comma followed by a space.
+ *      separated by a comma followed by a space.
  *
  *Return: Always 0.
  */
 int main(void)
 {
 	unsigned long per_num = 0, next_num = 1, sum = 0;
+	int final_res = 0;
 
 	for (;;)
 	{
+		sum = per_num + next_num;
 		if (sum > 4000000)
 		{
 			break;
 		}
 
-		sum = per_num + next_num;
-		printf("%lu, ", sum);
+		if (sum % 2 == 0)
+		{
+			final_res += sum;
+		}
+
 		per_num = next_num;
 		next_num = sum;
 	}
 
+	printf("%d", final_res);
 	return (0);
 }
