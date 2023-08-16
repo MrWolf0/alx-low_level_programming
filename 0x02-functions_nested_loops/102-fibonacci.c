563b1b390edbebbbd57f9e807b0bb6dd21cef44a
MrWolf0 <nassermessi33@gmail.com>
2023-08-16 22:17:35 +0300

    functions and nested loop

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - start point of the program
+ *
+ * Return: always must be 0 in case you are using int if you using void
+ *
+ *  no need to return keyword
+ */
+int main(void)
+{
+int long sum, per_num, next_num;
+int counter;
+next_num = 1;
+for (counter = 1; counter < 49; counter++)
+{
+printf("%ld, ", next_num);
+sum = per_num + next_num;
+per_num = next_num;
+next_num = sum;
+}
+sum = per_num + next_num;
+printf("%ld\n", sum);
+return (0);
+}
+
