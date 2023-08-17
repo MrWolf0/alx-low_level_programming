d23d1acac180bdf9d4e5d23ca857d19a0d386ce4
MrWolf0 <nassermessi33@gmail.com>
2023-08-17 15:14:25 +0300

    functions and nested loop

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,30 +1,32 @@
 #include "main.h"
-
 /**
- * print_diagonal - Draws a diagonal using the character \
- * @n: The number of times to print the char
+ * print_diagonal - Draws a diagonal line using the \ character.
+ * @n: The number of \ characters to be printed.
  */
 void print_diagonal(int n)
- {
- int times;
- int new_line;
-if(n > 0)
 {
-for (times = 0; times < n; times++)
-{
-for (new_line = 0; new_line < times; new_line++)
-{
-_putchar(' ');
-}
-_putchar('\\');
-/*stop befor but a new line at the end of loop*/
-if (times == n - 1)
-{
-break;
-}
-_putchar('\n');
-}
-}
-_putchar('\n');
- }
+	int times;
+	int new_line;
 
+	if (n > 0)
+	{
+		for (times = 0; times < n; times++)
+		{
+			for (new_line = 0; new_line < times; new_line++)
+			{
+				_putchar(' ');
+			}
+
+			_putchar('\\');
+			/*stop befor but a new line at the end of loop*/
+			if (times == n - 1)
+			{
+				break;
+			}
+
+			_putchar('\n');
+		}
+	}
+
+	_putchar('\n');
+}
