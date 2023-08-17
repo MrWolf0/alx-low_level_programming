da44f690f5b9da073ef9d1b7c9cd39a008cd02db
MrWolf0 <nassermessi33@gmail.com>
2023-08-17 15:03:45 +0300

    functions and nested loop

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,20 +1,22 @@
 #include "main.h"
 
 /**
- *print_numbers - print numbers from 0 to 9 using _puchar() expect 2,4
+ *print_most_numbers - print numbers from 0 to 9 using _puchar() expect 2,4
  *Return: Always 0.
  */
 void print_most_numbers(void)
 {
-int counter;
-for(counter = 0; counter <= 9; counter++)
-{
-if(counter +'0' == '2' || counter + '0' == '4')
-{
-continue;
-}
-_putchar(counter + '0');
-}
-_putchar('\n');
-}
+	int counter;
 
+	for (counter = 0; counter <= 9; counter++)
+	{
+		if (counter + '0' == '2' || counter + '0' == '4')
+		{
+			continue;
+		}
+
+		_putchar(counter + '0');
+	}
+
+	_putchar('\n');
+}
