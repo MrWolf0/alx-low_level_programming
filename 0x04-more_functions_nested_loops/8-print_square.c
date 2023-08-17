b3daa952c4a591a19c2ee2546a64ce648eb0df77
MrWolf0 <nassermessi33@gmail.com>
2023-08-17 15:00:53 +0300

    functions and nested loop

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *print_square -print # n* n times for n times sperated by new line
+ *such as 2 * 2 here total # is 4 and number of lines 2
+ *@size: The number of times to print the char
+ */
+void print_square(int size)
+{
+	int counter_1 = 0;
+	int counter_2 = 0;
+
+	while (counter_1 < size)
+	{
+		for (counter_2 = 0; counter_2 < size; counter_2++)
+		{
+			_putchar('#');
+		}
+
+		counter_1++;
+		_putchar('\n');
+	}
+}
---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - print fizz and buzz
+*
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int counter;
+	int multi = 3 * 5;
+
+	for (counter = 1; counter <= 100; counter++)
+	{
+		if (counter % multi == 0)
+		{
+			printf("%s ", "FizzBuzz");
+		}
+		else if (counter % 3 == 0)
+		{
+			printf("%s ", "Fizz");
+		}
+		else if (counter % 5 == 0)
+		{
+			printf("%s ", "Buzz");
+		}
+		else
+		{
+			printf("%d ", counter);
+		}
+	}
+
+	return (0);
+}
