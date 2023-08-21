40389b581f26e7bac0a41d5f4f3406a8eb05b1db
MrWolf0 <nassermessi33@gmail.com>
2023-08-21 17:38:19 +0300

    Pointers, arrays and strings task

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -6,15 +6,13 @@
  * @src: string to be copied
  *Return: Always 0.
  */
-char *_strcpy(char *dest, const char *src)
+char *_strcpy(char *dest, char *src)
 {
-	int index = 0;
-
-	while (src[index])
-	{
-		dest[index] = src[index];
-		index++;
-	}
-
-	return (dest);
+int counter = 0;
+while (src[counter])
+{
+dest[counter] = src[counter];
+counter++;
+}
+return (dest);
 }
