a64fdf9346f0d46692f8b9b8c63d17f3a8a57a64
MrWolf0 <nassermessi33@gmail.com>
2023-08-22 00:06:06 +0300

    Pointers, arrays and strings task

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,10 +1,9 @@
 #ifndef WOLF_H_INCLUDED
 #define WOLF_H_INCLUDEDs
-#include <stdlib.h>
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-size_t _strlen(const char *str);
+int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
