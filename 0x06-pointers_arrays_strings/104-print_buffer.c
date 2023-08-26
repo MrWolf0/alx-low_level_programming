f5d37646d4a3075d28d8c87d4c60d5c4c95063d7
MrWolf0 <nassermessi33@gmail.com>
2023-08-26 15:59:52 +0300

    Pointers, arrays and strings task

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -11,15 +11,25 @@
 void print_buffer(char *b, int size)
 {
 	int byte, index;
-
+/*the master loop start from 0 to 10 bytes only */
+/*here the loop check all buffer content and print only*/
+/*10 bytes per round untile finsh buffer contet*/
 	for (byte = 0; byte < size; byte += 10)
 	{
+/*show the position of bytes in memory in formate hex*/
+/*using 8 fromate and this position will increament by 10*/
 		printf("%08x: ", byte);
-
+/*this loop to move *p index by index through 10 bytes*/
+/*assume in first 10 bytes from 0 to 10 we have 10 characters*/
+/*this loop to itrate 10 characters one by one*/
 		for (index = 0; index < 10; index++)
 		{
 			if ((index + byte) >= size)
 				printf("  ");
+/*here *(p+index+byte) mean print the value */
+/*of the character in the index 0 in the first 10 bytes that */
+/*exist in the address that *p point for */
+/*for round 1 and so on until byte < size */
 
 			else
 				printf("%02x", *(b + index + byte));
@@ -27,7 +37,13 @@ void print_buffer(char *b, int size)
 			if ((index % 2) != 0 && index != 0)
 				printf(" ");
 		}
-
+/*this loop to divided 10 bytes 2 by 2 that mean */
+/*we will have 5 coulmns each coulmn start from */
+/*ASCII 32 to 126 ignore all character after escape character \*/
+/*by printing . instated of escape characte */
+/* to avoid buffer overflow just break the loop because */
+/*if you note if byte = 120 and the index 9*/
+/*became 129 > 126 the total size of buffer in memory in bytes*/
 		for (index = 0; index < 10; index++)
 		{
 			if ((index + byte) >= size)
@@ -40,9 +56,6 @@ void print_buffer(char *b, int size)
 			else
 				printf(".");
 		}
-
-		if (byte >= size)
-			continue;
 
 		printf("\n");
 	}
