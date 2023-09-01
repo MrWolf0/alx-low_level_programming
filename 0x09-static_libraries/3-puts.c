151f2c42f884fa424eb5e89d8897376ef46e5982
MrWolf0 <nassermessi33@gmail.com>
2023-09-01 19:09:47 +0300

    0x09. C - Static libraries

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,19 +1,12 @@
 #include "main.h"
-/**
- * _puts -print a giving string to stout device.
- *@str:  is a pointer point to the address of current index in a giving string
- *Return: Always 0.
- */
-void _puts(char *str)
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+void _puts(char *s)
 {
-/*impelement a variable act as a curser to move through the string*/
-/*index by index putting the current index to _putchar function*/
-/*this loop will stop if the current index = null terminator*/
-int counter = 0;
-while (str[counter] != '\0')
-{
-_putchar(str[counter]);
-counter++;
+    puts(s);
 }
-_putchar('\n');
-}
+
+
