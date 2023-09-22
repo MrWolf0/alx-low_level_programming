bfc54670be6a6df8e454d0ed270452417299920b
MrWolf0 <nassermessi33@gmail.com>
2023-09-22 12:34:48 +0300

    Update

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,6 +8,8 @@
  */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
+/*using __attribute__((__unused__) is to tell the compiler*/
+/*ignore that argument i will not use it in my code*/
 	printf("%s\n", argv[0]);
 
 	return (0);
