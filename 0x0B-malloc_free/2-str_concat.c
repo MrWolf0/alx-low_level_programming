3885eaf288ce546eef3892cbe775684e81059079
MrWolf0 <nassermessi33@gmail.com>
2023-09-22 21:32:08 +0300

    update str_concat file

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,78 +1,38 @@
-/*
- * ============================================================================
- *Name        : concatenate-strings.c
- *Author      : Mr Wolf
- *Version     :
- *Copyright   : GNU
- *Description : Hello World in C, Ansi-style
- *============================================================================
- */
 #include"main.h"
 #include <stdlib.h>
-
-int calculate_length(char *s);
 /**
- *str_concat - Concatenates two strings.
- *@s1: The string to be concatenated upon.
- *@s2: The string to be concatenated to s1.
+ * str_concat - Concatenates two strings.
+ * @s1: The string to be concatenated upon.
+ * @s2: The string to be concatenated to s1.
  *
- *Return: If concatenation fails - NULL.
- *Otherwise - a pointer the newly-allocated space in memory
- *containing the concatenated strings.
+ * Return: If concatenation fails - NULL.
+ *         Otherwise - a pointer the newly-allocated space in memory
+ *                     containing the concatenated strings.
  */
-
 char *str_concat(char *s1, char *s2)
 {
-	char *new_string;
-	int frist_length, counter = 0, secon_lengh, total_amount_mem = 0;
-	frist_length = calculate_length(s1);
-	secon_lengh = calculate_length(s2);
-	total_amount_mem = frist_length + secon_lengh;
-	new_string = malloc(sizeof(char) *total_amount_mem);
-	if (s1 == NULL)
-	{
+	char *concat_str;
+	int index, concat = 0, len = 0;
+
+	if (s1 == 0)
 		s1 = "";
-	}
-	 if (s2 == NULL)
-	{
+
+	if (s2 == 0)
 		s2 = "";
-	}
-	if (new_string == NULL)
-	{
-		return (NULL);
-	}
 
-	for (counter = 0; s1[counter]; counter++)
-	{
-		new_string[counter] = s1[counter];
-	}
+	for (index = 0; s1[index] || s2[index]; index++)
+		len++;
 
-	for (counter = 0; s2[counter]; counter++)
-	{
-		new_string[frist_length++] = s2[counter];
-	}
+	concat_str = malloc(sizeof(char) * len);
 
-	return (new_string);
-
-}
-
-/**
- *calculate_length - calculate length of a given string
- *@s: the string we need to calculate its length
- *Return: length of a given string
- **/
-int calculate_length(char *s)
-{
-	int counter, length;
-	if (!s)
-	{
+	if (concat_str == 0)
 		return (0);
-	}
 
-	for (counter = 0; s[counter]; counter++)
-	{
-		length++;
-	}
+	for (index = 0; s1[index]; index++)
+		concat_str[concat++] = s1[index];
 
-	return (length);
+	for (index = 0; s2[index]; index++)
+		concat_str[concat++] = s2[index];
+
+	return (concat_str);
 }
