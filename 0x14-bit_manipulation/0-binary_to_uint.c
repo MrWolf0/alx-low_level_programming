446d0008245d2fdc42a9fca155db32bafc02c211
MrWolf0 <nassermessi33@gmail.com>
2023-09-29 15:57:12 +0300

    Bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -19,36 +19,47 @@ int power(int power, int number);
  */
 unsigned int binary_to_uint(const char *b)
 {
+/*the final result*/
 	unsigned int result = 0;
-	int counter = 0;
+/* variable to calculate the length*/
+	int length = 0;
+/*variable to access the string in descending order*/
 	int index = 0;
+/*hold the value of 0 or 1 as integer not as a char*/
 	int converted;
+/*hode the number of index to pass to the power fun as the power*/
 	int place;
-
+/*if null value passed to the function*/
 	if (b == NULL)
 	{
 		return (0);
 	}
 	/*calculate length of the string*/
-	while (b[counter] != '\0')
+	while (b[length] != '\0')
 	{
-		counter++;
+		length++;
 	}
 
 	/*accessing our string from the last index in descending order*/
 	/*note that we must start from index 0 but stop before the last index*/
 	/*assume that we have a string with 3 indices length is 3*/
 	/*start from 1,2 and 3 so we need access index 0*/
-	/*to do that use counter-1*/
-	for (index = counter - 1; index >= 0; index--)
+	/*to do that use length-1*/
+	for (index = length - 1; index >= 0; index--)
 	{
-		place = (counter - 1) - index;
+/*here its important to specify where ones in the string*/
+/*so if you specify ones you can calculate the decimal value*/
+/*by using the rule 2 power of the index where i found the value 1*/
+		place = (length - 1) - index;
+/*check if any value expect 0 or 1 passed to the function*/
 		if (b[index] != '0' && b[index] != '1')
 		{
 			return (0);
 		}
-
+/*hold 0 and 1 as integers its optional we can compare as '0' or '1'*/
 		converted = (b[index] - '0');
+/*specify where ones only and pass the index of 1 to the power function*/
+/*calculate only 2 power index where 1 found*/
 		if (converted == 1)
 		{
 			result += power(2, place);
@@ -67,7 +78,10 @@ unsigned int binary_to_uint(const char *b)
 
 int power(int power, int number)
 {
-	int i; /*Variable used in loop counter */
+/*Variable used in loop counter */
+	int i;
+/*assign it to 1 even power = 0 it will retuern 1 as */
+/*number power 0 = 1*/
 	int result = 1;
 
 	for (i = 0; i < number; ++i)
