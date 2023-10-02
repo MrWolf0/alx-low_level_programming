bb32f4c340d2fe19da29980543e780a16b9f1b5a
MrWolf0 <nassermessi33@gmail.com>
2023-10-02 17:42:32 +0300

    0x15. C - File I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -22,36 +22,29 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-
-	ssize_t open_f, read_f, write_t;
-	char *ptr;
+	ssize_t o, r, w;
+	char *buffer;
 
 	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
+		free(buffer);
 		return (0);
 	}
 
-	ptr = malloc(sizeof(char) * letters);
-	if (ptr == NULL)
-	{
-		return (0);
-	}
+	free(buffer);
+	close(o);
 
-	open_f = open(filename, O_RDONLY);
-
-	read_f = read(open_f, ptr, letters);
-
-	write_t = write(1, ptr, read_f);
-
-	if (open_f == -1 || read_f == -1 || write_t == -1 || write_t != read_f)
-	{
-		free(ptr);
-		return (0);
-	}
-
-	/*close the connection after free the ptr */
-	free(ptr);
-	close(open_f);
-
-	return (write_t);
+	return (w);
+}
 }
