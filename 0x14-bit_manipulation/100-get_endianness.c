370a803159d764d58ed523b15e24c0aeef9d89d0
MrWolf0 <nassermessi33@gmail.com>
2023-11-24 13:00:31 +0200

    crackme3

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,24 @@
+/*
+ *============================================================================
+ * Name        : bits_manipulation.c
+ * Author      : MrWolf0
+ * Version     : 1.0
+ * Copyright   : GNU
+ * Description : 0-binary_to_uint.c
+ ============================================================================
+ */
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * get_endianness - check the code
+ *
+ * Return: Always 0.
+ */
+int get_endianness(void)
+{
+/* In computer science endianness is a way to repesent bytes */
+/* in the memory big-endian and little-endian */
+/* are the two main ways to represent endianness.*/
+
+}
