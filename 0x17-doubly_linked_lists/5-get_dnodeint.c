9df84663770c57b6c1b55a3598672586c253e1f6
MrWolf0 <nassermessi33@gmail.com>
2024-01-11 23:04:17 +0200

    double linkedlist

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -9,12 +9,19 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
+/* This loop is to find and print the number inside the node */
+/* While index > 0 mean when TRUE */
+/* If index == 0 mean false */
 	while (index)
 	{
-		if(head == NULL)
+		if (head == NULL)
 		{
 			return (NULL);
 		}
+/* Here we move node by node and if the index 5 */
+/* It will  decreament the index until it = 0*/
+/* then ending the loop return the data inside */
+/* the node that has number of index */
 		head = head->next;
 		index--;
 	}
