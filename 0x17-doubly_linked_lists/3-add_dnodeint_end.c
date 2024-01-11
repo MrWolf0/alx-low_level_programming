72b2a09c770aca59389dd416bf8fd53fb25efaa3
MrWolf0 <nassermessi33@gmail.com>
2024-01-11 21:27:17 +0200

    double linkedlist

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+
 /**
  * add_dnodeint_end - Adds a new node at the end of a dlistint_t list.
  * @head: A pointer to the head of the dlistint_t list.
@@ -9,31 +10,27 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node;
-	dlistint_t *last_node;
+	dlistint_t *new, *last;
 
-	last_node = malloc(sizeof(dlistint_t));
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-	{
-		printf("%s", "Invalid memory allocation");
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
 		return (NULL);
-	}
-	new_node->n = n;
-	new_node->next = NULL;
+
+	new->n = n;
+	new->next = NULL;
+
 	if (*head == NULL)
 	{
-		new_node->prev = NULL;
-		*head = new_node;
-		return (new_node);
+		new->prev = NULL;
+		*head = new;
+		return (new);
 	}
-		last_node = *head;
-		while (last_node->next != NULL)
-		{
-			last_node = last_node->next;
-		}
-		last_node->next = new_node;
-		new_node->prev = last_node;
-		return (new_node);
-}
 
+	last = *head;
+	while (last->next != NULL)
+		last = last->next;
+	last->next = new;
+	new->prev = last;
+
+	return (new);
+}
