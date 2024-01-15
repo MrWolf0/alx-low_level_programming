29b17d50d35d6ea2c4bc1f82ba44d4ca6776b39a
MrWolf0 <nassermessi33@gmail.com>
2024-01-15 21:30:11 +0200

    Dymanic link lib

---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,14 @@
+#include "test.h"
+/**
+ *  mod - divided two integer values
+* @num1:  is first integer
+* @num2:  is second integer
+ * Return: reminder
+ */
+int mod(int num1, int num2)
+{
+int result;
+result = num1 %num2;
+return (result);
+}
+
---
+++ 0x18-dynamic_libraries/mul.c
@@ -0,0 +1,13 @@
+#include "test.h"
+/**
+ *mul - multiply 2 number
+ *@a: is first number
+ *@b: is second number
+ *Return: Always 0.
+ */
+int mul(int a, int b)
+{
+int mul_result;
+mul_result = a * b;
+return (mul_result);
+}
