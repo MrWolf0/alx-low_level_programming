617fa9ecec3aa633be5acdc97662777c0072ffcf
MrWolf0 <nassermessi33@gmail.com>
2024-01-15 21:23:36 +0200

    Dymanic link lib

---
+++ 0x18-dynamic_libraries/test/sub.c
@@ -0,0 +1,14 @@
+#include "test.h"
+/**
+ *  sub - sub two integer values
+* @num1:  is first integer
+* @num2:  is second integer
+ * Return: result.
+ */
+int sub(int num1, int num2)
+{
+int result;
+result = num1 + num2;
+return (result);
+}
+
